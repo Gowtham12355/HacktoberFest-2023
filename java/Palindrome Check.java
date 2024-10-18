ee6abbb89659932fda7e2e92cee1b5dcb7bfc67e
Gowtham12355 <155940936+Gowtham12355@users.noreply.github.com>
2024-10-18 22:20:11 +0530

    Create Palindrome Check.java
    i have create palindrome ckecker for string in java

---
+++ java/Palindrome Check.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        if (isPalindrome(inputString)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        String cleanedString = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleanedString.length() - 1;
+
+        while (left < right) {
+            if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
